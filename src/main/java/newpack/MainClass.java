c9ed90dfbcfb6fa23682e036b0875affeaad0aa9
haji <skhadar275201@gmail.com>
2024-07-06 10:55:11 +0530

    third commit

---
+++ src/main/java/newpack/MainClass.java
@@ -0,0 +1,8 @@
+package newpack;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Your application logic here
+    }
+}
